2c1a8b8cdf5603a02d847319caf87d5312c6662a
qq <0qqw0qqw@gmail.com>
2020-11-28 08:34:10 +0300

    switch to tmux-ressurect

--- config/nvim/lspconfig.lua
+++ config/nvim/lspconfig.lua
@@ -1,8 +1,37 @@
 vim.cmd('packadd! nvim-lspconfig')
-function dummy()
-	print 'hi'
-end
 local lspconfig = require'nvim_lsp'
-lspconfig.pyls.setup{}
+local on_attach = function(_, bufnr)
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+		vim.lsp.diagnostic.on_publish_diagnostics, {
+			-- disable virtual text
+			virtual_text = false,
+
+			-- show signs
+			signs = true,
+
+			-- delay update diagnostics
+			update_in_insert = false,
+		}
+		)
+	require'completion'.on_attach()
+
+	local opts = { noremap=true, silent=true }
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>sk', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>swa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>swr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>swl', '<cmd>lua vim.lsp.buf.list_workspace_folders()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>srn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>se', '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>sl', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+end
+lspconfig.pyls.setup{ on_attach = on_attach }
 lspconfig.rnix.setup{}
 lspconfig.yamlls.setup{}
--- home.nix
+++ home.nix
@@ -252,7 +252,7 @@
     "super + F5" = "${config.dots.binDir}/change_brightness +5%";
 
     # terminal
-    "super + Return" = "${pkgs.xst}/bin/xst -e ${config.dots.binDir}/mx";
+    "super + Return" = "${pkgs.xst}/bin/xst -e ${pkgs.tmux}/bin/tmux";
     "super + shift + Return" = "${pkgs.xst}/bin/xst";
 
     # program launcher
@@ -797,11 +797,21 @@
     baseIndex = 1;
     escapeTime = 0;
     historyLimit = 50000;
-    tmuxp.enable = true;
     keyMode = "vi";
     plugins = with pkgs.tmuxPlugins; [
       {
         plugin = fzf-tmux-url;
+      }
+      {
+        plugin = resurrect;
+        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '15'
+        '';
       }
     ];
     extraConfig = builtins.readFile "${config.dots.confDir}/tmux/tmux.conf";
