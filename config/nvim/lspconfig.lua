c49c148141a5253dd3fbfa86c7a6d4e9d8d91817
qq <0qqw0qqw@gmail.com>
2021-02-03 15:35:56 +0300

    nvim: add format buffer keymap

--- config/nvim/lspconfig.lua
+++ config/nvim/lspconfig.lua
@@ -28,6 +28,7 @@ local on_attach = function(_, bufnr)
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>sl', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>fb', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 lspconfig.pyls.setup{ on_attach = on_attach }
 lspconfig.rnix.setup{ on_attach = on_attach }
