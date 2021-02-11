a2181101fd8d626bd662c05f1d3e5aa7e492f0ef
qq <0qqw0qqw@gmail.com>
2021-02-11 10:07:58 +0300

    nix: switch to flake version

--- configuration.nix
+++ configuration.nix
@@ -18,10 +18,10 @@
 
 
   nix = {
-    # package = pkgs.nixUnstable;
-    # extraOptions = ''
-    #   experimental-features = nix-command flakes
-    # '';
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
     autoOptimiseStore = true;
     trustedUsers = [ "root" "${config.dots.userName}" ];
   };
