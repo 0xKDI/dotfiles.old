f9a2a81e8b19d6afd6402fa19fcb3c051a944f56
qq <0qqw0qqw@gmail.com>
2021-02-21 20:42:57 +0300

    fix pythonrc

--- modules/python.nix
+++ modules/python.nix
@@ -8,5 +8,20 @@
   ];
 
 
-  xdg.configFile."pythonrc.py".source = "${config.dots.confDir}/pythonrc";
+  xdg.configFile."pythonrc.py".text = ''
+    #!/usr/bin/env python3
+
+    import atexit
+    import os
+    import readline
+
+    histfile = os.path.join(os.path.expanduser("~"), ".local/share/python_history")
+    try:
+        readline.read_history_file(histfile)
+        readline.set_history_length(1000)
+    except FileNotFoundError:
+        pass
+
+    atexit.register(readline.write_history_file, histfile)
+  '';
 }
