7c68399ce09d026cf3e6ffd873929b39ca5d9cb0
qq <0qqw0qqw@gmail.com>
2021-04-06 09:49:23 +0300

    rm direnv hacks

--- configs/default.nix
+++ configs/default.nix
@@ -174,26 +174,10 @@ in
         "--color=always {}"
       ];
     };
-    direnv =
-      let
-        alias_dir = "$PWD/.direnv/aliases";
-        alias_target = "${alias_dir}/$name";
-      in
-      {
-        enableZshIntegration = true;
-        enableNixDirenvIntegration = true;
-        stdlib = ''
-          export_alias() {
-          local name=$1
-          shift
-          mkdir -p "${alias_dir}"
-          PATH_add "${alias_dir}"
-          echo "#!/usr/bin/env -S bash -e" > "${alias_target}"
-          echo "$@" >> "${alias_target}"
-          chmod +x "${alias_target}"
-          }
-        '';
-      };
+    direnv = {
+      enableZshIntegration = true;
+      enableNixDirenvIntegration = true;
+    };
     starship = {
       enableZshIntegration = true;
       settings = {
