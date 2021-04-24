dca631db52e0f37f54168ba0d60183498c97c429
qq <0qqw0qqw@gmail.com>
2021-04-24 16:19:31 +0300

    Automatically import modules

--- modules/default.nix
+++ modules/default.nix
@@ -1,10 +1,13 @@
 { config, pkgs, lib, ... }:
 
+with lib;
+let
+  # Automatically import all *.nix files
+  # except default.nix in current dir
+  filter = k: v: k != "default.nix" && hasSuffix ".nix" k;
+  filteredFiles = filterAttrs filter (builtins.readDir ./.);
+  moduleFiles = mapAttrsToList (k: v: ./. + "/${k}") filteredFiles;
+in
 {
-  imports = [
-    ./aws.nix
-    ./python.nix
-    ./latex.nix
-    ./sxhkd.nix
-  ];
+  imports = moduleFiles;
 }
