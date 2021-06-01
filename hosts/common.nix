c53e23bae4f854b18fa6e5b1def0fa143e957667
Dmitry Kulikov <0qqw0qqw@gmail.com>
2021-06-01 13:27:54 +0300

    dunst: decrease default icon size

--- hosts/common.nix
+++ hosts/common.nix
@@ -731,6 +731,7 @@ in
         monitor = 0;
         follow = "mouse";
         geometry = "0x5-30+50";
+        max_icon_size = 25;
         indicate_hidden = "yes";
         shrink = "no";
         transparency = 0;
